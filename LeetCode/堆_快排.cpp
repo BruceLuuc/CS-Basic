78a31d59ba75fa8f775da0d6a3071ab33ec84e7e
Bruce <BruceLuu007@gmail.com>
2018-06-22 23:22:29 +0800

    排序算法总结

---
+++ LeetCode/堆_快排.cpp
@@ -0,0 +1,250 @@
+#include<iostream>
+#include<cassert>
+#include<algorithm>
+#include<ctime>
+#include <string>
+#include <cmath>
+#include<vector>
+using namespace std;
+//E盘视频教程 整理
+template<typename T>
+class MaxHeap {
+public:
+	MaxHeap(int capacity) {
+		data = new T[capacity + 1];
+		count = 0;
+		this->capacity = capacity;
+	}
+	MaxHeap(vector<T>&a) {
+		data = new T[a.size() + 1];
+		capacity = a.size();
+		for (int i = 0; i < a.size(); i++)
+			data[i + 1] = a[i];
+		count = a.size();
+		for (int i = count / 2; i >= 1; i--)
+			shiftDown(i);
+	}
+	~MaxHeap() { delete[] data; }
+	int size() { return count; }
+	bool isEmpty() { return count == 0; }
+	void insert(T item) { 
+		assert(count + 1 <= capacity);
+		data[count + 1] = item; 
+		count++; 
+		shiftUp(count);
+	}
+	T extractMax() {
+		assert(count > 0);
+		T res = data[1];
+		swap(data[1], data[count]);
+		count--;
+		shiftDown(1);
+		return res;
+	}
+	void print() {
+		cout << "The heap size is:"<<size()<<endl;
+		cout << "data in heap:";
+		if (count) {
+			for (int i=1;i<=count;i++)
+				cout << data[i] << "  ";
+		}
+		cout << endl;
+		int n = size();
+		int max_level = 0;
+		int number_per_level = 1;
+		while (n > 0) {
+			max_level += 1;
+			n -= number_per_level;
+			number_per_level *= 2;
+		}
+
+		int max_level_number = int(pow(2, max_level - 1));
+		int cur_tree_max_level_number = max_level_number;
+		int index = 1;
+		for (int level = 0; level < max_level; level++) {
+			string line1 = string(max_level_number * 3 - 1, ' ');
+
+			int cur_level_number = min(count - int(pow(2, level)) + 1, int(pow(2, level)));
+			bool isLeft = true;
+			for (int index_cur_level = 0; index_cur_level < cur_level_number; index++, index_cur_level++) {
+				putNumberInLine(data[index], line1, index_cur_level, cur_tree_max_level_number * 3 - 1, isLeft);
+				isLeft = !isLeft;
+			}
+			cout << line1 << endl;
+
+			if (level == max_level - 1)
+				break;
+
+			string line2 = string(max_level_number * 3 - 1, ' ');
+			for (int index_cur_level = 0; index_cur_level < cur_level_number; index_cur_level++)
+				putBranchInLine(line2, index_cur_level, cur_tree_max_level_number * 3 - 1);
+			cout << line2 << endl;
+
+			cur_tree_max_level_number /= 2;
+		}
+	}
+private:
+	T * data;
+	int count;
+	int capacity;
+	void shiftUp(int k) {
+		while(k>1 && data[k / 2] < data[k]) {
+			swap(data[k / 2], data[k]);
+			k /= 2;
+		}
+	}
+	void shiftDown(int k) {
+		while (2 * k <= count) {
+			int j = 2 * k;
+			if (j + 1 <= count && data[j + 1] > data[j])
+				j += 1;
+			if (data[k] >= data[j])
+				break;
+			swap(data[k], data[j]);
+			k = j;
+		}
+	}
+	void putNumberInLine(int num, string &line, int index_cur_level, int cur_tree_width, bool isLeft) {
+
+		int sub_tree_width = (cur_tree_width - 1) / 2;
+		int offset = index_cur_level * (cur_tree_width + 1) + sub_tree_width;
+		assert(offset + 1 < line.size());
+		if (num >= 10) {
+			line[offset + 0] = '0' + num / 10;
+			line[offset + 1] = '0' + num % 10;
+		}
+		else {
+			if (isLeft)
+				line[offset + 0] = '0' + num;
+			else
+				line[offset + 1] = '0' + num;
+		}
+	}
+
+	void putBranchInLine(string &line, int index_cur_level, int cur_tree_width) {
+
+		int sub_tree_width = (cur_tree_width - 1) / 2;
+		int sub_sub_tree_width = (sub_tree_width - 1) / 2;
+		int offset_left = index_cur_level * (cur_tree_width + 1) + sub_sub_tree_width;
+		assert(offset_left + 1 < line.size());
+		int offset_right = index_cur_level * (cur_tree_width + 1) + sub_tree_width + 1 + sub_sub_tree_width;
+		assert(offset_right < line.size());
+
+		line[offset_left + 1] = '/';
+		line[offset_right + 0] = '\\';
+	}
+};
+
+
+//使用堆排序
+template<typename T>
+void heapSort1(vector<T>&a) {
+	MaxHeap<T>heap(a.size());
+	for (int i = 0; i < a.size(); i++)
+		heap.insert(a[i]);
+	for (int i = a.size() - 1; i >= 0; i--)
+		a[i] = heap.extractMax();
+}
+template<typename T>
+void heapSort2(vector<T>&a) {
+	MaxHeap<T>heap(a);
+	for (int i = a.size() - 1; i >= 0; i--)
+		a[i] = heap.extractMax();
+}
+
+//原地堆排序
+template<typename T>
+void __shiftDown(vector<T>&a,int n, int k) {
+	while (2 * k+1 < n) {
+		int j = 2 * k+1;
+		if (j + 1 < n && a[j + 1] > a[j])
+			j += 1;
+		if (a[k] >= a[j])
+			break;
+		swap(a[k], a[j]);
+		k = j;
+	}
+}
+template<typename T>
+void heapSort(vector<T>&a) {
+	//heapify
+	int n = a.size();
+	for (int i = (n - 1) / 2; i >= 0; i--)
+		__shiftDown(a, n,i);
+	for (int i = n - 1; i > 0; i--) {
+		swap(a[0], a[i]);
+		__shiftDown(a,i, 0);
+	}
+}
+
+
+
+
+
+
+
+
+//快速排序
+//返回P，使得a[l,……,p-1]<a[p];a[p+1,……,r]>a[p]
+template<typename T>
+int __partition(vector<T>&a, int l, int r) {
+	swap(a[l], a[rand() % (r - l + 1) + l]);
+	T v = a[l];//选第一个为枢纽
+	int j = l;
+	for (int i = l + 1; i <= r; i++) {
+		if (a[i] < v)
+			swap(a[++j], a[i]);		
+	}
+	swap(a[l], a[j]);
+	return j;
+}
+template<typename T>
+void __quickSort(vector<T>&a, int l, int r) {
+	if (l >= r)return;
+	int p = __partition(a, l, r);
+	__quickSort(a, l, p - 1);
+	__quickSort(a, p + 1, r);
+}
+template<typename T>
+void quickSort(vector<T>&a) {
+	srand(time(nullptr));
+	__quickSort(a, 0, a.size() - 1);
+}
+
+
+
+
+
+
+
+
+
+int main1() {
+	MaxHeap<int>heap(100);
+
+	srand(time(nullptr));
+	for (int i = 0; i < 15; i++)
+		heap.insert(rand() % 100);
+
+	heap.print();
+
+	while (!heap.isEmpty()) {
+		cout << heap.extractMax() << "  ";
+	}
+
+	cout <<endl<< "测试堆排序：" << endl;
+	vector<int>x;
+	for (int i = 0; i < 15; i++)
+		x.push_back(rand() % 100);
+	for (auto i : x)
+		cout << i << "  ";
+	cout << endl;
+
+	quickSort(x);
+	for (auto i : x)
+		cout << i << "  ";
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ LeetCode/排序算法总结.cpp
@@ -0,0 +1,320 @@
+#include<iostream>
+#include<vector>
+#include<cassert>
+#include<algorithm>
+#include<ctime>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+
+
+//冒泡排序 稳定 O(N^2)
+//把最大的沉到数组末尾
+template<typename T>
+void Bubble_Sort(vector<T>&a){
+	int len = a.size();
+	int i, j;
+	for (i = 0; i < len - 1; i++)
+		for (j = 0; j < len - 1 - i; j++)
+			if (a[j] > a[j + 1])			
+				swap(a[j], a[j + 1]);
+}
+//把最小的浮出到数组开头
+template<typename T>
+void BubbleSort(vector<T>&a){
+	int len = a.size();
+	int i, j;
+	for (i = 0; i < len - 1; i++)
+		for (j = len - 1; j >i; --j)
+			if (arr[j] < arr[j - 1])
+				swap(arr[j], arr[j - 1]);
+}
+//冒泡排序的改进：鸡尾酒排序
+template<typename T>
+void CocktailSort(vector<T>&a){
+	int left = 0;                            // 初始化边界
+	int right = a.size() - 1;
+	while (left < right){
+		// 前半轮,将最大元素放到后面	
+		for (int i = left; i < right; i++){   	
+			if (a[i] > a[i + 1])
+				swap(a[i] , a[i + 1]);
+		}
+		right--;
+		// 后半轮,将最小元素放到前面
+		for (int i = right; i > left; i--){   		
+			if (a[i - 1] > a[i])
+				swap(a[i - 1] , a[i]);
+		}
+		left++;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//选择排序 O(N^2) 不稳定
+template<typename T>
+void selectionSort(vector<T>&a){
+	int n = a.size();
+	for (int i = 0; i != n - 1; ++i){
+		int minIndex = i;
+		for (int j = i + 1; j != n; ++j)
+			if (arr[j] < arr[minIndex])
+				minIndex = j;
+		if (i != minIndex)//减少自身交换
+			swap(arr[i], arr[minIndex]);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//插入排序 平均O(N^2) 稳定
+//填坑，把前面比他大的排好序的元素依次往后移动一个元素，直到遇到前面一个数比他小 然后把i插入坑里面
+//如果输入数据预先排序，运行时间为O(N），因为内层循环的检测总是立即判定不成立而终止。
+//用于少量元素的排序
+template<typename T>
+void insertionSort(vector<T>&a) {
+	for (int i = 1; i<a.size(); i++) {		
+		T tmp = a[i];
+		int j;
+		for (j = i; j > 0 && tmp<a[j - 1]; j--)
+			a[j] = a[j - 1];
+		a[j] = tmp;
+	}
+}
+//插入排序的高效改进：希尔排序(Shell Sort)
+//不稳定
+//可能有更好的增量
+template<typename T>
+void ShellSort(vector<T>&a) {
+	for (int gap = a.size() / 2; gap != 0; --gap) {
+		for (int i = gap; i<a.size(); i++) {
+			T tmp = a[i];
+			int j=i;
+			for (; j >= gap && tmp<a[j - gap]; j-=gap)
+				a[j] = a[j - gap];
+			a[j] = tmp;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+//归并排序(Merge Sort) 稳定
+// 最差时间复杂度 ---- O(N*logN)
+// 最优时间复杂度 ---- O(N*logN)
+// 平均时间复杂度 ---- O(N*logN)
+template<typename T>
+void mergeSort(vector<T>&a) {
+	vector<T>tmpArray(a.size());
+	mergeSort(a,tmpArray, 0, a.size() - 1);
+}
+template<typename T>
+void mergeSort(vector<T>&a,vector<T>&tmpArray,int left,int right) {	
+	if (left == right) return;// 当待排序的序列长度为1时，递归开始回溯，进行merge操作
+
+	int center = (left + right) / 2;
+	mergeSort(a, tmpArray, left, center);
+	mergeSort(a, tmpArray, center + 1, right);
+	merge(a, tmpArray, left, center + 1, right);	
+}
+template <typename T>
+void merge(vector<T> & a, vector<T> & tmpArray,	int leftPos, int rightPos, int rightEnd){
+	int leftEnd = rightPos - 1;
+	int tmpPos = leftPos;
+	int numElements = rightEnd - leftPos + 1;
+
+	// Main loop
+	while (leftPos <= leftEnd && rightPos <= rightEnd)
+		if (a[leftPos] <= a[rightPos])
+			tmpArray[tmpPos++] = a[leftPos++];
+		else
+			tmpArray[tmpPos++] = a[rightPos++];
+
+	while (leftPos <= leftEnd)    // Copy rest of first half
+		tmpArray[tmpPos++] = a[leftPos++];
+
+	while (rightPos <= rightEnd)  // Copy rest of right half
+		tmpArray[tmpPos++] = a[rightPos++];
+
+	// Copy tmpArray back
+	for (int i = 0; i < numElements; i++, rightEnd--)
+		a[rightEnd] = tmpArray[rightEnd];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//堆排序(Heap Sort) 不稳定
+//O(N*logN)
+template<typename T>
+void __shiftDown(vector<T>&a, int n, int k) {
+	while (2 * k + 1 < n) {
+		int j = 2 * k + 1;
+		if (j + 1 < n && a[j + 1] > a[j])
+			j += 1;
+		if (a[k] >= a[j])
+			break;
+		swap(a[k], a[j]);
+		k = j;
+	}
+}
+template<typename T>
+void heapSort(vector<T>&a) {
+	//heapify
+	int n = a.size();
+	for (int i = (n - 1) / 2; i >= 0; i--)
+		__shiftDown(a, n, i);
+	for (int i = n - 1; i > 0; i--) {
+		swap(a[0], a[i]);
+		__shiftDown(a, i, 0);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+//快速排序(Quick Sort) 不稳定
+// 最差 ---- 每次选取的基准都是最大（或最小）的元素
+//导致每次只划分出了一个分区，需要进行n-1次划分才能结束递归，时间复杂度为O(N^2)
+
+//最优 ---- 每次选取的基准都是中位数,这样每次都均匀的划分出两个分区
+//只需要logN次划分就能结束递归，时间复杂度为O(N*logN)
+
+// 平均时间复杂度 ---- O(N*logN)
+template<typename T>
+void insertionSort(vector<T>&a,int left,int right) {
+	for (int i = left+1; i<=right; i++) {
+		T tmp = a[i];
+		int j;
+		for (j = i; j > left && tmp<a[j - 1]; j--)
+			a[j] = a[j - 1];
+		a[j] = tmp;
+	}
+}
+template<typename T>
+const T& median3(vector<T>&a, int left, int right) {
+	int center = (left + right) / 2;
+	if (a[center] < a[left])swap(a[center], a[left]);
+	if (a[right] < a[left])swap(a[right], a[left]);
+	if (a[right] < a[center])swap(a[right], a[center]);
+	swap(a[center], a[right - 1]);
+	return a[right - 1];
+}
+template<typename T>
+void quickSort(vector<T>&a, int left, int right) {
+	if (right - left > 10) {
+		T pivot = median3(a, left, right);
+		int i = left + 1;
+		int j = right - 2;
+		for (;;) {
+			while (a[i] < pivot)i++;
+			while (pivot < a[j])j++;
+			if (i < j)swap(a[i], a[j]);
+			else break;
+		}
+		swap(a[i], a[right - 1]);//把主元放到分界线
+		quickSort(a, left, i - 1);
+		quickSort(a, i + 1, right);
+	}
+	else
+		insertionSort(a, left, right);
+}
+template<typename T>
+void quickSort(vector<T>&a) {
+	quickSort(a, 0, a.size() - 1);
+}
+
+
+
+
+
+
+//测试用例
+int main2() {
+	
+
+	srand(time(nullptr));
+
+	cout << endl << "快排：" << endl;
+	vector<int>x;
+	for (int i = 0; i < 100; i++)
+		x.push_back(rand() % 1000);
+
+	for (int i=0 ;i<20;i++ )
+		cout << x[i] << "  ";
+	cout << endl;
+
+	clock_t startTime = clock();
+	quickSort(x);
+	clock_t endTime = clock();
+	cout << " 耗费时间: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+
+	for (int i = 0; i<20; i++)
+		cout << x[i] << "  ";
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+
+/*
+花费一晚上，集中时间把排序算法梳理了一遍，参考资料如下：
+[1]  Mark Allen Weiss. 数据结构与算法分析 C++描述(第3版)[M]. 人民邮电出版社, 2007.
+[2]  Adam Drozdek. C++数据结构与算法(第4版)[M]. 清华大学出版社, 2014.
+[3]  常用排序算法总结(一) http://www.cnblogs.com/eniac12/p/5329396.html#s5
+[4]  常用排序算法总结(二) http://www.cnblogs.com/eniac12/p/5332117.html
+*/
