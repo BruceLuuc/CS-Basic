3353114294cbe292c47e4167bbe9d5f0f45d7525
Bruce <BruceLuu007@gmail.com>
2018-09-09 15:46:20 +0800

    7

--- LeetCode/7. Reverse Integer.cpp
+++ LeetCode/7. Reverse Integer.cpp
@@ -1,4 +1,4 @@
-#include"x.h"
+﻿#include"x.h"
 
 class Solution_7_1 {
 public:
@@ -10,32 +10,10 @@ public:
 		for (; t; t /= 10)
 			r = 10 * r + t % 10;
 
-		bool sign = x > 0 ? false : true;
-		if (r > INT_MAX || (sign && r > INT_MAX)) {
+		bool negative = x < 0 ? true : false;
+		if (r > INT_MAX || negative && r > INT_MAX)
 			return 0;
-		}
-		else {
-			if (sign) {
-				return -r;
-			}
-			else {
-				return r;
-			}
-		}
+		else
+			return negative ? -r : r;
 	}
 };
-
-
-int main_7()
-{
-	int a = -1234560;
-	int c = a & 1;
-	int d = 10 & 1;
-	int e = 9 & 1;
-
-	Solution_7_1 b;
-	std::cout << b.reverse(a) << std::endl;
-	std::cout << c << "," << d << "," << e << "\n";
-	system("pause");
-	return 0;
-}
