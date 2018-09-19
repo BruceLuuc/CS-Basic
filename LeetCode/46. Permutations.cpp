511b7e15fb6d7f9aa4dad213c719d671c0620cb5
Bruce <BruceLuu007@gmail.com>
2018-09-19 20:29:29 +0800

    commit

--- LeetCode/46. Permutations.cpp
+++ LeetCode/46. Permutations.cpp
@@ -17,13 +17,16 @@ Output:
 ]
 
 */
+//31. Next Permutation
 class Solution_next {
 public:
 	void nextPermutation(vector<int>& nums) {
 		int n = nums.size(), i = n - 2, j = n - 1;
-		while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+		while (i >= 0 && nums[i] >= nums[i + 1]) 
+			--i;
 		if (i >= 0) {
-			while (nums[j] <= nums[i]) --j;
+			while (nums[j] <= nums[i]) 
+				--j;
 			swap(nums[i], nums[j]);
 		}
 		reverse(nums.begin() + i + 1, nums.end());
--- LeetCode/_classical_problems.cpp
+++ LeetCode/_classical_problems.cpp
@@ -27,3 +27,59 @@ int maxSubArray(const vector<int>& nums) {
 	}
 	return result;
 }
+
+/*
+全排列LeetCode#46
+*/
+
+void nextPermutation(vector<int>& nums) {
+	int n = nums.size(), i = n - 2, j = n - 1;
+	while (i >= 0 && nums[i] >= nums[i + 1])
+		--i;
+	if (i >= 0) {
+		while (nums[j] <= nums[i])
+			--j;
+		swap(nums[i], nums[j]);
+	}
+	reverse(nums.begin() + i + 1, nums.end());
+}
+class Solution_next_permutation {
+public:
+	void nextPermutation(vector<int> &nums) {
+		next_permutation(nums.begin(), nums.end());
+	}
+	template<typename BidiIt>
+	bool next_permutation(BidiIt first, BidiIt last) {
+		// Get a reversed range to simplify reversed traversal.
+		const auto rfirst = reverse_iterator<BidiIt>(last);//改变迭代器方向
+		const auto rlast = reverse_iterator<BidiIt>(first);
+		// Begin from the second last element to the first element.
+		auto pivot = next(rfirst);
+		// Find `pivot`, which is the first element that is no less than its
+		// successor. `Prev` is used since `pivort` is a `reversed_iterator`.
+		while (pivot != rlast && *pivot >= *prev(pivot))
+			++pivot;
+		// No such elemenet found, current sequence is already the largest
+		// permutation, then rearrange to the first permutation and return false.
+		if (pivot == rlast) {
+			reverse(rfirst, rlast);
+			return false;
+		}
+		// Scan from right to left, find the first element that is greater than `pivot`.
+		auto change = find_if(rfirst, pivot, bind1st(less<int>(), *pivot));
+		swap(*change, *pivot);
+		reverse(rfirst, pivot);
+		return true;
+	}
+};
+class Solution_46 {
+public:
+	vector<vector<int>> permuteUnique(vector<int>& num) {
+		vector<vector<int> > result;
+		sort(num.begin(), num.end());
+		do {
+			result.push_back(num);
+		} while (next_permutation(num.begin(), num.end()));
+		return result;
+	}
+};
